72362d29972da012885a5aeef20dbb3987adba79
PulkitSaxena <pulkit.saxena93@gmail.com>
2018-09-26 23:11:50 +0530

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://PulkitSaxena:maverick93@ds121182.mlab.com:21182/blog_dev',
+  mongoURI: 'connection_string',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379'
 };
